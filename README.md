9fe3b01d69e3a2d70ae98533a911823950e5b09e
stordahl <jacob@blkcatstudio.com>
2021-02-11 21:15:34 -0600

    Adding plugins as deps; all tests passed

---
+++ README.md
@@ -0,0 +1,5 @@
+# steel-studio
+
+this is the repo for the one-click starter Sanity Studio for the Steel Headless E-commerce sudo framework. 
+
+Currently in development...⚠️
--- template/dashboardConfig.js
+++ template/dashboardConfig.js
@@ -3,5 +3,19 @@ export default {
     {
       name: 'project-users'
     },
+    {
+      name: 'snipcart-bestsellers',
+      options: {
+        apiKey: '',
+        limit: 5,
+      },
+    },
+    {
+      name: 'snipcart-orders',
+      options: {
+        apiKey: '',
+        limit: 5,
+      },
+    },
   ]
 }
